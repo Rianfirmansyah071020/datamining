5bd50bb74b8811312875e3326f8bd91d3bc472b6
Rianfirmansyah071020 <rian071020@gmail.com>
2023-07-20 11:06:17 +0700

    awal

---
+++ footer.php
@@ -0,0 +1,10 @@
+<div class="footer">
+    <div class="footer-inner">
+        <div class="footer-content">
+            <span class="bigger-120">
+                <span class="blue bolder">Data Mining</span>
+                Apriori Bengkel Mustofa Motor&copy; 2023 | Copyright by <a href="https://blogbugabagi.blogspot.com" target="_blank" rel="noopener noreferrer">Bintang Januaweko</a>
+            </span>
+        </div>
+    </div>
+</div>
